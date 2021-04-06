cdf5d719f3dbb82a402f8613316556a815636be6
Gaël Sudour-Jeannin <gael.sudourjeannin@gmail.com>
2021-04-06 14:58:02 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logo acs</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <div class= "container">
+
+    &#60&nbsp;<span class="accesscode">Access&nbsp;<span class="code">Code</span></span>&nbsp;&#62
+        <div class="school">School</div>
+
+    </div>
+
+</body>
+
+</html>
